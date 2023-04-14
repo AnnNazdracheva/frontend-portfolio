9ecb7f609070cade707cd36f8e06dcfa80ddd4c3
Anna Nazdracheva <109549011+AnnNazdracheva@users.noreply.github.com>
2023-04-14 10:42:56 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// Menu
+
+document.addEventListener('click', documentActions);
+
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('[data-goto]')) {
+    const goTo = targetElement.closest('[data-goto]').dataset.goto;
+    const goToElement = document.querySelector(goTo);
+    const headerHeight = document.querySelector('.header').offsetHeight;
+
+    if(goToElement) {
+      window.scrollTo({
+        top: goToElement.offsetTop - headerHeight,
+        behavior: "smooth",
+      });
+    }
+    e.preventDefault();
+  }
+}
